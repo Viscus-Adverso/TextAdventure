d819a6b19b7cce9a1f1506d1c726f1368dd52795
Viscus <keegansaint194@hotmail.com>
2016-09-15 12:02:38 -0400

    added comand system

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  */
 public class Game {
     static Scanner scanner = new Scanner(System.in);
+    static Player player = new Player();
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome, traveller!");
@@ -12,22 +13,47 @@ public class Game {
         boolean keepRunning = true;
 
         while (keepRunning) {
-            Player player = new Player();
             player.chooseName();
             player.chooseWeapon();
             player.chooseLocation();
             player.findItem("shield");
             player.findItem("potion");
 
+
             System.out.println("You win!");
             System.out.println("Would you like to play again? Type y or n");
 
-            String answer = scanner.nextLine();
+            String answer = customLine();
             if (answer.equalsIgnoreCase("n")){
                 keepRunning = false;
             }
         }
 
         System.out.println("Game over.");
+    }
+
+    static String customLine() {
+        String line = scanner.nextLine();
+        while (line.startsWith("/")) {
+            switch (line) {
+                case "/exit":
+                    System.exit(0);
+                    break;
+                case "/inv":
+                    for (String item : player.items) {
+                        System.out.println(item);
+                    }
+                    if (player.items.isEmpty()) {
+                        System.out.println("You have no items.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid command!");
+                    break;
+
+            }
+            line = scanner.nextLine();
+        }
+        return line;
     }
 }
